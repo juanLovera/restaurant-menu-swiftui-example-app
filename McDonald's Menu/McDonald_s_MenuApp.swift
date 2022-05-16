9ac3ae30a0cc92c824aef59c2fd45cba8c05c5e3
Juan Lovera <juanloverab@gmail.com>
2022-05-16 13:53:48 -0300

    Initial Commit

---
+++ McDonald's Menu/McDonald_s_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  McDonald_s_MenuApp.swift
+//  McDonald's Menu
+//
+//  Created by Juan Jose Lovera on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct McDonald_s_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
